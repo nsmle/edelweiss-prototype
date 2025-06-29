8a7837048e0c034338c99d42fd04f6e7a08333cb
nsmle <fikiproductionofficial@gmail.com>
2025-06-29 09:23:51 +0700

    chore(wip): seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
+    import { page } from '$app/state';
     import Header from '$lib/header.svelte';
     import '../app.css';
 
-    let { children } = $props();
+    let props = $props();
+    let { children } = props;
 </script>
+
+<svelte:head>
+    <meta name="url" content={page?.url?.href} />
+
+    <meta property="og:title" content="Edelweiss Photobooth" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={page?.url?.href} />
+    <meta property="og:image" content={`${page?.url?.origin}/brand.svg`} />
+    <meta property="og:site_name" content="Edelweiss Photobooth" />
+    <meta property="og:description" content="Ciptakan moment dan abadikan kenangan" />
+    <meta property="og:locale" content="id_ID" />
+    <meta property="og:locale:alternate" content="en_US" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="Edelweiss Photobooth" />
+    <meta name="twitter:title" content="Edelweiss Photobooth" />
+    <meta name="twitter:description" content="Edelweiss Photobooth" />
+    <meta name="twitter:image" content={`${page?.url?.origin}/brand.svg`} />
+    <meta name="twitter:image:src" content={`${page?.url?.origin}/brand.svg`} />
+</svelte:head>
 
 <Header />
 
