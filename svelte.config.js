7e61e291991af33a2c33783aa21aa333fe4bb271
nsmle <fikiproductionofficial@gmail.com>
2025-07-05 19:53:42 +0700

    fix: seo

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
                   images: {
                       sizes: [640, 828, 1200, 1920, 3840],
                       formats: ['image/avif', 'image/webp'],
-                      minimumCacheTTL: 300,
+                      minimumCacheTTL: 604800,
                       domains: ['dicea.fun', 'www.dicea.fun', 'edelweiss-beta.vercel.app']
                   }
               })
