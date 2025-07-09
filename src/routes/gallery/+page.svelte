1a9fea2ffdf42aea2bbafc0434a55c45e74bec46
nsmle <fikiproductionofficial@gmail.com>
2025-07-10 05:46:57 +0700

    chore: change gradient background color

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -102,7 +102,7 @@
         aria-hidden="true"
     >
         <div
-            class="-trangray-x-1/3 relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 rotate-20 bg-linear-to-tr from-green-400 to-purple-500 opacity-20 sm:left-[calc(50%-20rem)] sm:w-288.75"
+            class="-trangray-x-1/3 relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 rotate-20 bg-linear-to-tr from-purple-400 to-green-500 opacity-20 sm:left-[calc(50%-20rem)] sm:w-288.75"
             style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
         ></div>
     </div>
